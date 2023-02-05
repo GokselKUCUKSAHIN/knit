2148ff13381d26ce793a4349d6eebc13be42cc2c
Göksel Küçükşahin <goksel.kucuksahin@trendyol.com>
2023-02-05 13:09:43 +0300

    feat: implement GetPointListOnTheLine method in high_level

--- knit/high_level/high_level.go
+++ knit/high_level/high_level.go
@@ -2,6 +2,7 @@ package high_level
 
 import (
 	"knit/knit/high_level/model"
+	"knit/knit/utils"
 	"math"
 )
 
@@ -23,35 +24,60 @@ func GeneratorPinList(length int, width, height float64) []*model.Vector {
 		angle := angleUnit*float64(i) - HalfPi
 		x := math.Round(centerX + radius*math.Cos(angle))
 		y := math.Round(centerY + radius*math.Sin(angle))
-		if x > width {
-			x -= 1
+		if x >= width {
+			x = width - 1
 		}
-		if y > height {
-			y -= 1
+		if y >= height {
+			y = height - 1
 		}
 		pins = append(pins, model.CreateVector(x, y))
 	}
 	return pins
 }
 
-// dotX, dotY, startX, startY, endX, endY float64
+func IsDotOnTheLine(dot, start, end *model.Vector) bool {
+	if utils.FloatEquals(start.X, end.X) {
+		return utils.FloatEquals(dot.X, end.X)
+	}
+	slope := start.Slope(end)
+	intercept := float64(start.Y) - slope*float64(start.X)
+	blockTopY := float64(dot.Y) + Half
+	blockBottomY := float64(dot.Y) - Half
+	blockLeftY := slope*(float64(dot.X)-Half) + intercept
+	blockRightY := slope*(float64(dot.X)+Half) + intercept
+	if math.Abs(slope) <= 1 {
+		return (blockLeftY >= blockBottomY && blockLeftY <= blockTopY) ||
+			(blockRightY >= blockBottomY && blockRightY <= blockTopY)
+	} else if slope > 0 {
+		return !(blockLeftY > blockTopY || blockRightY < blockBottomY)
+	} else {
+		return !(blockRightY > blockTopY || blockLeftY < blockBottomY)
+	}
+}
 
-//func IsDotOnTheLine(dot, start, end *model.Vector) bool {
-//	if utils.FloatEquals(start.X, end.X) {
-//		return utils.FloatEquals(dot.X, end.X)
-//	}
-//	slope := start.Slope(end)
-//	intercept := float64(start.Y) - slope*float64(start.X)
-//	blockTopY := dot.Y + Half
-//	blockBottomY := dotY - Half
-//	blockLeftY := slope*(dotX-Half) + intercept
-//	blockRightY := slope*(dotX+Half) + intercept
-//	if math.Abs(slope) <= 1 {
-//		return (blockLeftY >= blockBottomY && blockLeftY <= blockTopY) ||
-//			(blockRightY >= blockBottomY && blockRightY <= blockTopY)
-//	} else if slope > 0 {
-//		return !(blockLeftY > blockTopY || blockRightY < blockBottomY)
-//	} else {
-//		return !(blockRightY > blockTopY || blockLeftY < blockBottomY)
-//	}
-//}
+func GetPointListOnTheLine(start, end *model.Vector) []*model.Vector {
+	pointList := make([]*model.Vector, 0)
+	var movementX, movementY int16
+	if end.X > start.X {
+		movementX = 1
+	} else {
+		movementX = -1
+	}
+	if end.Y > start.Y {
+		movementY = 1
+	} else {
+		movementY = -1
+	}
+	currentX := start.X
+	currentY := start.Y
+	for i := int16(0); (currentX != end.X || currentY != end.Y) && i < 1000; i++ {
+		pointList = append(pointList, model.CreateVector(currentX, currentY))
+		if IsDotOnTheLine(model.CreateVector(currentX+movementX, currentY), start, end) {
+			currentX += movementX
+		} else {
+			currentY += movementY
+		}
+	}
+	pointList = append(pointList, end)
+	return pointList
+}
--- main.go
+++ main.go
@@ -2,25 +2,32 @@ package main
 
 import (
 	"fmt"
-	"knit/knit/low_level"
+	"knit/knit/high_level"
 )
 
 func main() {
-	pinList := low_level.GeneratePinList(400, 600, 600)
-	counts := make([]int, 0)
-	for i := 0; i < len(pinList)/2; i++ {
-		xA := pinList[2*i]
-		yA := pinList[2*i+1]
-		for j := 0; j < i; j++ {
-			xB := pinList[2*j]
-			yB := pinList[2*j+1]
-			count := low_level.GetPointListOnLine(xA, yA, xB, yB)
-			counts = append(counts, len(count))
-		}
-	}
-	sum := 0
-	for _, count := range counts {
-		sum += count
-	}
-	fmt.Printf("sum: %d, count: %d, avg: %.2f\n", sum, len(counts), float64(sum)/float64(len(counts)))
+	pinList := high_level.GeneratorPinList(400, 600, 600)
+	pinS := pinList[201]
+	pinE := pinList[305]
+	fmt.Println(pinS)
+	fmt.Println(pinE)
+	pointList := high_level.GetPointListOnTheLine(pinS, pinE)
+	fmt.Println(pointList)
+	fmt.Println(len(pointList))
+	//counts := make([]int, 0)
+	//for i := 0; i < len(pinList)/2; i++ {
+	//	xA := pinList[2*i]
+	//	yA := pinList[2*i+1]
+	//	for j := 0; j < i; j++ {
+	//		xB := pinList[2*j]
+	//		yB := pinList[2*j+1]
+	//		count := low_level.GetPointListOnLine(xA, yA, xB, yB)
+	//		counts = append(counts, len(count))
+	//	}
+	//}
+	//sum := 0
+	//for _, count := range counts {
+	//	sum += count
+	//}
+	//fmt.Printf("sum: %d, count: %d, avg: %.2f\n", sum, len(counts), float64(sum)/float64(len(counts)))
 }
